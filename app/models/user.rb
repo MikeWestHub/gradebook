39f29829a66ef88c613eac995db2ff559e680513
Mike West <westmd23@gmail.com>
2016-04-20 16:38:23 -0400

    setup active record relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true 
+  validates :name, presence: true
   validates :login, presence: true, uniqueness: true
   validates :password, confirmation: true, presence: true
+  has_one :teacher
+  has_one :student
+  has_one :parent
   has_secure_password
 end
